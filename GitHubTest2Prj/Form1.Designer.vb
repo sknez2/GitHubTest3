e42964e8d3858f46d4953e7f2c55b17c2c9ebc25
sknez2 <sylwester_knez@yahoo.co.uk>
2013-05-21 14:43:04 +0200

    commit 2 on branch master

--- GitHubTest2Prj/Form1.Designer.vb
+++ GitHubTest2Prj/Form1.Designer.vb
@@ -24,6 +24,8 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -44,11 +46,31 @@ Partial Class Form1
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Wersja druga"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(420, 60)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(141, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "==> sknez"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(420, 126)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(141, 29)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "==> sknez2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(616, 278)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -59,5 +81,7 @@ Partial Class Form1
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
