4433e08b6fb970c102b7d24480ff3cc4a244d93c
sknez2 <sylwester_knez@yahoo.co.uk>
2013-05-21 17:35:07 +0200

    dodanie komunikatu (w gałęzi master)

--- GitHubTest2Prj/Form1.vb
+++ GitHubTest2Prj/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        MsgBox("tu będzie wywołanie sknez2")
+    End Sub
 End Class
